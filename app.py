f592c4de0cc3c545374a7d0cd1bc5d1833b9c698
Chandan Sohi <91230351+sohic@users.noreply.github.com>
2024-04-23 16:04:36 -0700

    updated to take parameter

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from uszipcode import SearchEngine
 
 app = Flask(__name__)
 
-@app.route("/zip", methods=['GET'])
-def get_zip():
+@app.route("/", methods=['GET'])
+def get_zip(zip):
     engine = SearchEngine()
-    zipcode = engine.by_zipcode(98275)
-    location_info = {"zipcode":zipcode.zipcode, "city":zipcode.major_city, "state":zipcode.state_abbr}
+    place = engine.by_zipcode(zip)
+    location_info = {"zipcode":place.zipcode, "city":place.major_city, "state":place.state_abbr}
     return jsonify(location_info)
 
 if __name__ == "__main__":
